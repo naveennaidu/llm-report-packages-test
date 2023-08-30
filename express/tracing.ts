846aa8d45d9339a506ca01bec608364c86cd6825
Naveen <mnaveennaidu9927@gmail.com>
2023-08-30 09:23:47 +0530

    moved code to express folder

--- express/tracing.ts
+++ express/tracing.ts
@@ -1,7 +1,7 @@
 import { llmReportSdk } from "llm-report";
 
 export const sdk = llmReportSdk(
-  "852ca30464bd8cb64200cb07087cc699ece14785bf9ab8be577af69dd1070a37",
+  "0937fbc4b13dd9cd8dd910bed1ae6000b99cbe7738b749fad857b51f5811c8d7",
   "http://localhost:3000/api/v1/log/openai"
 );
 sdk.start();
